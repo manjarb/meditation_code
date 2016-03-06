8408b774e706c86867e188e9ca8c702fa99deb8d
manjarb <manjarb4@gmail.com>
2016-03-07 00:54:41 +0800

    update email formal

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
+require 'mail'
+
 class ApplicationMailer < ActionMailer::Base
-  default from: "admin@thaimeditationcenter.com"
+
+
+  address = Mail::Address.new("admin@thaimeditationcenter.com") # ex: "john@example.com"
+  address.display_name = "Thaimeditationcenter".dup # ex: "John Doe"
+# Set the From or Reply-To header to the following:
+  address.format # returns "John Doe <john@example.com>"
+
+  default from: address
+
   layout 'mailer'
+
 end
