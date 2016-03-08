5c6958411a658287e19110b62f6fe9c3697e083a
manjarb <manjarb4@gmail.com>
2016-03-08 23:50:35 +0800

    update sidekiq

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,7 +1,9 @@
 class HardWorker
   include Sidekiq::Worker
-  def perform(name, count)
+  def perform(user_id)
     # do something
     #HardWorker.perform_async('bob', 5)
+    user_active = User.find_by(id: user_id)
+    UserMailer.reservation_details_mailer(user_active).deliver_now
   end
 end
